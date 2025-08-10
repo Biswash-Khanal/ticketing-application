7a1b678937523a53430a7d961febf7e5d6634725
biswash-khanal <kbiswash47@gmail.com>
2025-08-10 16:54:43 +0545

    color imports, text styles, and overall basic frontend setup

---
+++ app/{components}/TicketCard.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Deleteblock from "./Deleteblock";
+import PriorityDisplay from "./PriorityDisplay";
+import ProgressBar from "./ProgressBar";
+import StatusDisplay from "./StatusDisplay";
+
+const TicketCard = () => {
+  return (
+    <div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
+      <div className="flex mb-3">
+        <PriorityDisplay />
+        <div className="ml-auto">
+          <Deleteblock />
+        </div>
+      </div>
+      <h4>Ticket Title</h4>
+      <hr className="h-px border-0 bg-page mb-2" />
+      <p className="whitespace-pre-wrap">
+        {" "}
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse adipisci
+        quidem natus voluptatum enim velit culpa veniam maxime quo quae optio
+        ipsum ipsa ut, asperiores provident? Qui sit aut aliquam!
+      </p>
+
+      <div className="flex-grow"></div>
+
+      <div className="flex mt-2">
+        <div className="flex flex-col ">
+            <p>08:31 22/12/2002</p>
+          <ProgressBar />
+        </div>
+
+        <div className="ml-auto flex items-end">
+            <StatusDisplay />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TicketCard;
