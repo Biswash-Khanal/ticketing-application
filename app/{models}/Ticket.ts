8fcace4f9df6f752712888a54a98ac68cf453ed4
biswash-khanal <kbiswash47@gmail.com>
2025-08-12 20:37:04 +0545

    api and database initial setup

---
+++ app/{models}/Ticket.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error(
+    "❌ Please define the MONGODB_URI environment variable in .env.local"
+  );
+}
+
+await mongoose.connect(MONGODB_URI);
+
+mongoose.Promise = global.Promise;
+
+const ticketSchema = new Schema(
+  {
+    title: String,
+    description: String,
+    category: String,
+    priority: Number,
+    progress: Number,
+    status: String,
+    active: Boolean,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Ticket = mongoose.models.Ticket || mongoose.model("Ticket", ticketSchema);
+
+
+export default Ticket;
