961e23d0fe80894e74d2e9e117ea791e5016f712
biswash-khanal <kbiswash47@gmail.com>
2025-08-15 20:09:03 +0545

    delete api initial

--- app/api/Tickets/route.ts
+++ app/api/Tickets/route.ts
@@ -25,10 +25,17 @@ export async function GET() {
 }
 
 
-export async function UPDATE(){
+export async function UPDATE(req:Request){
   try {
-    const 
+    const body = await req.json();
+    const ticketData = body.formData;
+    await Ticket.create(ticketData);
+
+    return NextResponse.json({ message: "Ticket Created" }, { status: 201 });
   } catch (error) {
-    
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
   }
 }
+
+
+export async function DELETE(req:Request)
